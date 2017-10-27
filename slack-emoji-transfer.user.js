70d1d30aad7b2665282c6a00751ea4f31a9a48c3
B Porter <beporter@users.sourceforge.net>
2017-10-27 11:56:51 -0700

    Import external dependencies, init vars.

--- slack-emoji-transfer.user.js
+++ slack-emoji-transfer.user.js
@@ -9,6 +9,7 @@
 // @grant          none
 // @run-at         document-end
 // @require        https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.js
+// @require        https://stuk.github.io/jszip-utils/dist/jszip-utils.js
 // ==/UserScript==
 
 /* jshint asi: true, esnext: true, -W097 */
@@ -16,7 +17,53 @@
 (function($) {
   'use strict'
 
+	var emojiRows = $('tr.emoji_row');
+	var images = [];
+	var aliases = [];
+
+
+
+
+
   // Code...
+
+// x Write as a Greasemonkey/Tampermonkey JS script that runs in-browser (at least for download.)
+//
+// x Import jQuery into the script for keeping things concise.
+//
+// x Import jszip library.
+//
+// x Store every DOM node matching `tr.emoji_row` into an array.
+//
+// x Create two storage objects: `images` and `aliases`.
+//
+// Iterate over the list:
+//
+// * Examine `td.custom_emoji_type.text()` and if it matches `/^Alias for /`:
+//   * Use the final `:word:` from `td.custom_emoji_type.text()` (the alias target) as the key.
+//   * Use `td.custom_emoji_name.text()` (the "name" of the alias) as the value.
+//   * Store the pair in `aliases`.
+// * Else:
+//   * Store `td.custom_emoji_name.text()` (the "name" the custom emoji) as the key.
+//   * Extract `data-original` (the image source URL) as the value.
+//   * Store the pair in `images`.
+//
+// Iterate over the `images` object:
+//
+// * Grab the file extension from the image URL.
+// * Craft a target filename using the emoji name (object key) plus the file extension.
+// * Trigger a file download of the given image URL (object value), saving to the target filename.
+//
+// Open a new browser window(?)
+//
+// Iterate over the `aliases` object:
+//
+// * Write the key and value to the window (as plain text) in a format such as `$key,$value\n`.
+//
+// Trigger a download(?) of the text file window, saving as `aliases.txt`.
+
+
+
 
 }).bind(this)(jQuery)
 
