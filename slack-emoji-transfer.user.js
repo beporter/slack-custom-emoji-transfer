4f233fddc470ec8dba6ab82996f0e90fe42b4a0d
B Porter <beporter@users.sourceforge.net>
2017-10-31 09:14:45 -0500

    ZIP file download WIP.

--- slack-emoji-transfer.user.js
+++ slack-emoji-transfer.user.js
@@ -5,24 +5,26 @@
 // @namespace      http://github.com/beporter/slack-custom-emoji-transfer
 // @version        0.0.1
 // @icon           http://tampermonkey.net/favicon.ico
-// @match          https://*.slack.com/admin/emoji*
 // @match          https://*.slack.com/customize/emoji*
+// @match          https://*.slack.com/admin/emoji*
+// @connect        https://*.slack-edge.com
 // @grant          none
 // @run-at         document-end
 // @require        https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.js
-// @require        https://stuk.github.io/jszip-utils/dist/jszip-utils.js
+// @require        https://stuk.github.io/jszip/dist/jszip.js
 // ==/UserScript==
 
 /* jshint asi: true, esnext: true, -W097 */
 
 (function($) {
-  'use strict'
+    'use strict'
 
-	var emojiRows = $('tr.emoji_row');
-	var images = {};
-	var aliases = {};
+	var images = Object.create(null);
+	var aliases = Object.create(null);
+    var zip = new JSZip();
 
-	emojiRows.each(function (key, item) {
+	// Collect emoji names and URLs/aliases.
+    $('tr.emoji_row').each(function (key, item) {
         var name = $('td[headers=custom_emoji_name]', item)[0].innerText;
         var text = $('td[headers=custom_emoji_type]', item)[0].innerText;
         var nameClean = name.match(/:([\w-]+):/)[1].trim();
@@ -35,60 +37,46 @@
         }
 	});
 
-
-    aliases.forEach(function (item) {
-        // write to a "text file"
-    });
-    // add the text file to a zip
-
-    images.forEach(function (item) {
-        // add to a zip
-    });
+    // Create a text file of alias mappings in the zip.
+    zip.file("aliases.txt",
+        Object.keys(aliases).map(function (key) {
+            return key + "," + aliases[key];
+        }).join("\n")
+    );
 
 console.log({
     aliases,
     images
 });
 
-  // Code...
+    // Fetch the binary data for each image and add it into the zip keyed by the emoji name.
+    Object.keys(aliases).map(function (name) {
+        var url = images[name];
+        var ext = url.slice((url.lastIndexOf(".") - 1 >>> 0) + 2);
+        var filename = name + '.' + ext;
+        var content = fetch(url);
 
-// x Write as a Greasemonkey/Tampermonkey JS script that runs in-browser (at least for download.)
-//
-// x Import jQuery into the script for keeping things concise.
-//
-// x Import jszip library.
-//
-// x Store every DOM node matching `tr.emoji_row` into an array.
-//
-// x Create two storage objects: `images` and `aliases`.
-//
-// x Iterate over the list:
-//
-// x Examine `td.custom_emoji_type.text()` and if it matches `/^Alias for /`:
-//   * Use the final `:word:` from `td.custom_emoji_type.text()` (the alias target) as the key.
-//   * Use `td.custom_emoji_name.text()` (the "name" of the alias) as the value.
-//   * Store the pair in `aliases`.
-// x Else:
-//   * Store `td.custom_emoji_name.text()` (the "name" the custom emoji) as the key.
-//   * Extract `data-original` (the image source URL) as the value.
-//   * Store the pair in `images`.
-//
-// Iterate over the `images` object:
-//
-// * Grab the file extension from the image URL.
-// * Craft a target filename using the emoji name (object key) plus the file extension.
-// * Trigger a file download of the given image URL (object value), saving to the target filename.
-//
-// Open a new browser window(?)
-//
-// Iterate over the `aliases` object:
-//
-// * Write the key and value to the window (as plain text) in a format such as `$key,$value\n`.
-//
-// Trigger a download(?) of the text file window, saving as `aliases.txt`.
+        fetch(url, {
+            mode: 'no-cors' //@WORKING: `no-cors` only works in Chrome Canary!!!
+        })
+            .then(function(response) {
+                return response.blob();
+            })
+            .then(function(myBlob) {
+                zip.file(filename, myBlob, {base64: true});
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    });
 
+return;
 
-
+    zip.generateAsync({type:"blob"})
+        .then(function(content) {
+            // see FileSaver.js
+            saveAs(content, $('#header_team_name').text() + ".zip");
+        });
 
 }).bind(this)(jQuery)
 
